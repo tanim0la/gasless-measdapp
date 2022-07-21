ca4214c0ba638304d6416d729866c8d90a0411b1
Tanim0la <36541366+tanim0la@users.noreply.github.com>
2022-07-21 16:53:08 +0000

    Update contract.js

--- ethereum/contract.js
+++ ethereum/contract.js
@@ -2,14 +2,14 @@
 
 import { ethers } from 'ethers'
 import compiledMessageV4 from './Message.json'
-require('dotenv').config()
+//require('dotenv').config()
 
 let pprovider
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
   pprovider = new ethers.providers.Web3Provider(window.ethereum)
 } else {
-  pprovider = new ethers.providers.JsonRpcProvider(process.env.NETWORK)
+  pprovider = new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/adaa638d09ba451589fc8a00235e3489')
 }
 
 const contract = new ethers.Contract(
